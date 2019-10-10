b086c753449b02ba5f0cd9d8cef47a4c0eb89112
shinmantis <shinmantis@users.noreply.github.com>
2019-10-10 10:26:51 -0600

    Added javascipt redirects to simulate backend login behavior

---
+++ source/javascript/index.js
@@ -0,0 +1,29 @@
+
+console.log("index.js loaded");
+
+$( "#login-form" ).submit(function(event)
+{
+  event.preventDefault();
+
+  console.log("redirecting...");
+   
+  window.location.replace("https://peanuts.com");
+});
+
+
+/*
+var example2 = new Vue({
+  el: '#login-controllers',
+  data: {
+    name: 'Vue.js'
+  },
+  // define methods under the `methods` object
+  methods: {
+    greet: function (event) {
+      // `this` inside methods points to the Vue instance
+      alert('Welcome back! Make sure you\'ve filled out all login fields');
+      // `event` is the native DOM event
+    }
+  }
+})
+*/

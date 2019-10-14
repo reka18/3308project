6f457e310a644f36aa48dd80f2adca6c97c1a21f
Reagan Karnes <reagan-karnes@pluralsight.com>
2019-10-14 03:00:44 -0600

    updated main go file

--- socialmediasite.go
+++ socialmediasite.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	// EXAMPLE USAGE
+	//AddNewUserAccount(40, "Rodrigo", "Garcia", "rigo.garcia@colorado.edu",
+	//	"M", true, "iamtheverymodelofthemodernmajorgeneral")
+	//AddNewUserAccount(36, "Reagan", "Karnes", "reagan.karnes@colorado.edu",
+	//	"M", true, "abcdefghijklmnopqrstuvwxyz1234567890")
+
+	// EXAMPLE USAGE
+	result := LoginUserAccount("reagan.karnes@colorado.edu", "abcdefghijklmnopqrstuvwxyz1234567890")
+	PrintUser(result)
+
 	fs := http.FileServer(http.Dir("source"))
 	http.Handle("/", fs)
 
@@ -16,3 +28,4 @@ func main() {
 		log.Fatal(err)
 	}
 }
+

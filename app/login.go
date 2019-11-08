ce92890464f9c101452f58ac5b84818f0c3eca10
Reagan Karnes <reagan-karnes@pluralsight.com>
2019-11-08 12:26:06 -0700

    fixed data parsing

--- app/login.go
+++ app/login.go
@@ -3,25 +3,30 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 )
 
 func userLoginGET(w http.ResponseWriter, r *http.Request) {
-	http.FileServer(http.Dir("css"))
+
 	t := template.Must(template.ParseFiles("web/login.html"))
 	_ = t.Execute(w, "")
+
 }
 
 func userLoginPOST(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "ParseForm() err: %v", err)
-		return
-	}
-	log.Printf("%s", r.FormValue("username"))
+
+	r.ParseForm()
+
+	fmt.Println("username:", r.Form["username"])
+	fmt.Println("password:", r.Form["pass"])
+
+	t := template.Must(template.ParseFiles("web/login.html"))
+	t.Execute(w, "")
+
 }
 
 func UserLoginHandler(w http.ResponseWriter, r *http.Request) {
+
 	push(w, "css/main.css")
 	push(w, "css/util.css")
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
@@ -30,7 +35,6 @@ func UserLoginHandler(w http.ResponseWriter, r *http.Request) {
 	case "GET":
 		userLoginGET(w, r)
 	case "POST":
-		userLoginGET(w, r)
 		userLoginPOST(w, r)
 	}
 

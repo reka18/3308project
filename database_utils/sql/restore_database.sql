1eabc6580bd95e015e238b9846e8a92708b59f3a
Reagan Karnes <reagan-karnes@pluralsight.com>
2019-11-13 09:47:37 -0700

    updates with proper password encryption

--- database_utils/sql/restore_database.sql
+++ database_utils/sql/restore_database.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 9.6.15
 -- Dumped by pg_dump version 9.6.15
 
--- Started on 2019-11-12 21:22:19 MST
+-- Started on 2019-11-13 09:47:03 MST
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -36,7 +36,7 @@ COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
 --
--- TOC entry 481 (class 1247 OID 148071)
+-- TOC entry 481 (class 1247 OID 148107)
 -- Name: gender; Type: TYPE; Schema: public; Owner: rkmac
 --
 
@@ -54,7 +54,7 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- TOC entry 185 (class 1259 OID 148077)
+-- TOC entry 188 (class 1259 OID 148130)
 -- Name: posts; Type: TABLE; Schema: public; Owner: rkmac
 --
 
@@ -71,7 +71,7 @@ CREATE TABLE public.posts (
 ALTER TABLE public.posts OWNER TO rkmac;
 
 --
--- TOC entry 186 (class 1259 OID 148080)
+-- TOC entry 187 (class 1259 OID 148128)
 -- Name: posts_id_seq; Type: SEQUENCE; Schema: public; Owner: rkmac
 --
 
@@ -87,7 +87,7 @@ ALTER TABLE public.posts_id_seq OWNER TO rkmac;
 
 --
 -- TOC entry 2419 (class 0 OID 0)
--- Dependencies: 186
+-- Dependencies: 187
 -- Name: posts_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: rkmac
 --
 
@@ -95,7 +95,7 @@ ALTER SEQUENCE public.posts_id_seq OWNED BY public.posts.id;
 
 
 --
--- TOC entry 187 (class 1259 OID 148082)
+-- TOC entry 186 (class 1259 OID 148115)
 -- Name: users; Type: TABLE; Schema: public; Owner: rkmac
 --
 
@@ -117,7 +117,7 @@ CREATE TABLE public.users (
 ALTER TABLE public.users OWNER TO rkmac;
 
 --
--- TOC entry 188 (class 1259 OID 148088)
+-- TOC entry 185 (class 1259 OID 148113)
 -- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: rkmac
 --
 
@@ -133,7 +133,7 @@ ALTER TABLE public.users_id_seq OWNER TO rkmac;
 
 --
 -- TOC entry 2420 (class 0 OID 0)
--- Dependencies: 188
+-- Dependencies: 185
 -- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: rkmac
 --
 
@@ -141,7 +141,7 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
--- TOC entry 2279 (class 2604 OID 148090)
+-- TOC entry 2280 (class 2604 OID 148133)
 -- Name: posts id; Type: DEFAULT; Schema: public; Owner: rkmac
 --
 
@@ -149,7 +149,7 @@ ALTER TABLE ONLY public.posts ALTER COLUMN id SET DEFAULT nextval('public.posts_
 
 
 --
--- TOC entry 2280 (class 2604 OID 148091)
+-- TOC entry 2279 (class 2604 OID 148118)
 -- Name: users id; Type: DEFAULT; Schema: public; Owner: rkmac
 --
 
@@ -157,8 +157,8 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 
 --
--- TOC entry 2407 (class 0 OID 148077)
--- Dependencies: 185
+-- TOC entry 2410 (class 0 OID 148130)
+-- Dependencies: 188
 -- Data for Name: posts; Type: TABLE DATA; Schema: public; Owner: rkmac
 --
 
@@ -168,7 +168,7 @@ COPY public.posts (id, userid, content, upvotes, downvotes, deleted) FROM stdin;
 
 --
 -- TOC entry 2421 (class 0 OID 0)
--- Dependencies: 186
+-- Dependencies: 187
 -- Name: posts_id_seq; Type: SEQUENCE SET; Schema: public; Owner: rkmac
 --
 
@@ -176,34 +176,30 @@ SELECT pg_catalog.setval('public.posts_id_seq', 1, false);
 
 
 --
--- TOC entry 2409 (class 0 OID 148082)
--- Dependencies: 187
+-- TOC entry 2408 (class 0 OID 148115)
+-- Dependencies: 186
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: rkmac
 --
 
 COPY public.users (id, age, firstname, lastname, email, username, public, joindate, active, password, gender) FROM stdin;
-1	35	Reagan	Karnes	reagan.karnes@colorado.edu	reka18	t	2019-11-12 15:59:43.930777	t	1234	M
-2	42	Rigo	Garcia	rigo.garcia@colorado.edu	riga69	t	2019-11-12 16:00:42.337296	t	1234	M
-3	26	Graham	Dominick	graham.dominick@colorado.edu	grdo42	t	2019-11-12 16:01:27.827039	t	1234	M
-4	1	Alex	Karnes	alex.kar@fam.com	alex01	t	2019-11-12 21:16:23.930953	t	1234	F
-5	5	Wes	Karnes	wes.kar@fam.com	wes05	t	2019-11-12 21:16:52.265622	t	1234	M
-6	37	Diyou	Karnes	di.kar@fam.com	dika14	t	2019-11-12 21:18:02.566631	t	1234	F
-7	10	Beverly	Pudin	dog.mail@fam.com	dog10	t	2019-11-12 21:18:59.927224	t	1234	F
-8	3	Luka	Mao	cat.mail@fam.com	cat03	t	2019-11-12 21:19:34.513244	t	1234	M
+1	35	Reagan	Karnes	reagan.karnes@colorado.edu	reka18	t	2019-11-13 09:43:55.646359	t	$2a$04$.iwEEhuoYAfnr8oeAU8X5eKP17DfrP1elZ9OmFT3PSYg7w.F/IiUS	M
+2	36	Diyou	Karnes	diyoubolical@icloud.com	dika14	t	2019-11-13 09:45:07.883697	t	$2a$04$e6aMPeV/XrNYag5EcweyOOai9RjuSB482xnqZqghsARQ30UA3I5aS	F
+3	5	Wesley	Karnes	weslogica@icloud.com	wes14	t	2019-11-13 09:45:50.043926	t	$2a$04$6q9dJq9UvZyOI/ydrs7bMegNxFP/FD/L3oEh4ydryEYgGnmqc/2rq	M
+4	1	Alexandria	Karnes	reaganomica@icloud.com	alex18	t	2019-11-13 09:46:30.636529	t	$2a$04$zstL.wI/rbvC.WHn4mhjpuK2EWZ7G9Sgb9wis4Xi1FBZakhuhjivW	F
 \.
 
 
 --
 -- TOC entry 2422 (class 0 OID 0)
--- Dependencies: 188
+-- Dependencies: 185
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: rkmac
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 8, true);
+SELECT pg_catalog.setval('public.users_id_seq', 4, true);
 
 
 --
--- TOC entry 2282 (class 2606 OID 148093)
+-- TOC entry 2288 (class 2606 OID 148135)
 -- Name: posts posts_pkey; Type: CONSTRAINT; Schema: public; Owner: rkmac
 --
 
@@ -212,7 +208,7 @@ ALTER TABLE ONLY public.posts
 
 
 --
--- TOC entry 2284 (class 2606 OID 148095)
+-- TOC entry 2282 (class 2606 OID 148125)
 -- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: rkmac
 --
 
@@ -221,7 +217,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 2286 (class 2606 OID 148097)
+-- TOC entry 2284 (class 2606 OID 148123)
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: rkmac
 --
 
@@ -230,7 +226,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 2288 (class 2606 OID 148099)
+-- TOC entry 2286 (class 2606 OID 148127)
 -- Name: users users_username_key; Type: CONSTRAINT; Schema: public; Owner: rkmac
 --
 
@@ -239,7 +235,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 2289 (class 2606 OID 148100)
+-- TOC entry 2289 (class 2606 OID 148136)
 -- Name: posts posts_userid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: rkmac
 --
 
@@ -247,7 +243,7 @@ ALTER TABLE ONLY public.posts
     ADD CONSTRAINT posts_userid_fkey FOREIGN KEY (userid) REFERENCES public.users(id);
 
 
--- Completed on 2019-11-12 21:22:19 MST
+-- Completed on 2019-11-13 09:47:03 MST
 
 --
 -- PostgreSQL database dump complete

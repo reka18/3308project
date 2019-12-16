459544dbe9817fc5a45a600db74005a7354ac3a0
shinmantis <H.Rodrigo.Garcia@protonmail.com>
2019-12-16 01:18:11 -0700

    Added reactToPost Ajax call wiring

--- web/js/user-crud-operations.js
+++ web/js/user-crud-operations.js
@@ -29,26 +29,9 @@ function reactToPost(reaction)
 
 function newPost()
 {
-    const postURL = getUsername()+ "/post";
-    const postData = document.getElementById('postText').value;
-    document.getElementById("postText").innerText="";
-
-    if(postData === "")
-    {
-        alert("Enter some text to post!");
-        return;
-    }
-
-    $.ajax({
-        type:'POST',
-        url: postURL,
-        success: function(responseData, status, responseObject)
-        {
-            $('.modal').click();
-            getPosts().then(function (results)
-            {
-                updatePosts(results);
-            });
+        const postURL = getUsername()+ "/post";
+        const postData = document.getElementById('postText').value;
+        document.getElementById("postText").innerText="";
 
         },
         data: {"Content-Type": "text/html; charset=utf-8", "content": postData},
@@ -60,7 +43,29 @@ function newPost()
             alert(ajaxOptions);
             $('.modal').click()
         }
-    });
+
+        $.ajax({
+                    type:'POST',
+                    url: postURL,
+                    success: function(responseData, status, responseObject)
+                    {
+                            $('.modal').click();
+                            getPosts().then(function (results)
+                            {
+                                updatePosts(results);
+                            });
+
+                    },
+                    data: {"Content-Type": "text/html; charset=utf-8", "content": postData},
+                    dataType: 'html',
+                    cache: false,
+                    error: function (xhr, ajaxOptions, thrownError) {
+                            alert(xhr.status);
+                            alert(thrownError);
+                            alert(ajaxOptions);
+                            $('.modal').click()
+                    }
+            });
 }
 
 async function getPosts()
@@ -130,12 +135,6 @@ function userSearch()
 
 
         });
-}
-
-
-function upVotePost(da)
-{
-
 }
 
 

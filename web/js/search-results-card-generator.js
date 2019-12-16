c1a042c43e844fc1cc1c585f104a56ba20d51caf
Reagan Karnes <reagan-karnes@pluralsight.com>
2019-12-16 16:16:18 -0700

    pull

--- web/js/search-results-card-generator.js
+++ web/js/search-results-card-generator.js
@@ -13,9 +13,9 @@ function search_results_card_generator(userObject)
                 <div class="search-results-user-details-container">
                     <div><h5 class="card-title card-title-plus">${userName}</h5><a href="#"><img class="search-results-follow-icon followMe" onclick="followUser(${unam})" class="search-results-follow-icon" src="images/follow-opt-w-512.png"/></a></div>
                     <div class="search-results-user-information-container">
-                        <span class="search-results-user-information">First name: ${userObject['User']['Firstname']} </span>
-                        <span class="search-results-user-information">Last name: ${userObject['User']['Lastname']} </span>
-                        <span class="search-results-user-information">Age: ${userObject['User']['Age']}</span>
+                        <span class="search-results-user-information">First name:  </span>
+                        <span class="search-results-user-information">Last name: ${} </span>
+                        <span class="search-results-user-information">Age: ${}</span>
                         <span class="search-results-user-information">Email:  ${userObject['User']['Email']}</span>
                         <span class="search-results-user-information">Join Date: ${userObject['User']['FriendlyJoinDate']}</span>
                         <span class="search-results-user-information">Gender: ${userObject['User']['Gender']}</span>
